b644799e72dd29eeed3c60d1d0b5229ab63082fb
Raphael J. Sandor <raphaelshreds@gmail.com>
2021-10-23 11:39:11 -0400

    Notes on Enums.

--- chapter1/enums/Manager.java
+++ chapter1/enums/Manager.java
@@ -3,6 +3,13 @@ public class Manager {
 		Season summer = Season.SUMMER;
 		summer.expectedVistors = "ok";
 		summer.printExpectedVisitors();
+
+		Season summer2 = Season.SUMMER;
+		/**
+			The expected vistors is static. 
+			We can think of enums as a special type of static class.
+		/*
+		summer2.printExpectedVisitors();
 	}
 
 }
