33fb4c09cc694b52083ade592b0e5d7c6a4e0a53
Raphael J. Sandor <raphaelshreds@gmail.com>
2021-10-23 11:29:53 -0400

    Chapter5 studying.

--- chapter5/zoneDateTime/LearningZonedDateTime.java
+++ chapter5/zoneDateTime/LearningZonedDateTime.java
@@ -7,7 +7,7 @@ import java.time.Month;
 public class LearningZonedDateTime {
 
 	public static void main(String args[]){
-		LocalDate date = LocalDate.of(2016, Month.NOVEMBER, 6);
+		LocalDate date = LocalDate.of(2021, Month.NOVEMBER, 7);
 		LocalTime time = LocalTime.of(1, 30);
 		ZoneId zone = ZoneId.of("US/Eastern");
 		ZonedDateTime dateTime1 = ZonedDateTime.of(date, time, zone);
@@ -21,7 +21,14 @@ public class LearningZonedDateTime {
 		int clock1 = dateTime1.getHour();
 		int clock2 = dateTime2.getHour();
 		System.out.println(hours + "," + clock1 + "," + clock2);
-
+		
+		LocalDate localDate = LocalDate.of(2017, 3, 12);
+		LocalTime localTime = LocalTime.of(2,0);
+		ZoneId zoneNY = ZoneId.of("America/New_York");
+		ZonedDateTime z = ZonedDateTime.of(localDate, localTime, zoneNY);
+		ZonedDateTime z2 = ZonedDateTime.of(localDate, LocalTime.of(3, 0), zoneNY);
+		System.out.println(z);
+		System.out.println(z2);
 	}
 
 
