6e7bf47fe43119d11373115226f2a8af123db7c8
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-06-25 06:25:18 -0400

    Notes on generics

--- OCP17/chapter9/Packager.java
+++ OCP17/chapter9/Packager.java
@@ -1,3 +1,11 @@
+/**
+ * Sample class to demonstrate various errors and gotchas of 
+ * generic programming.
+ */
+
+import java.util.List; 
+import java.util.ArrayList;
+
 class Packaging { } 
 class Bag extends Packaging {}
 class Envelope extends Bag {}
@@ -5,9 +13,27 @@ class Box extends Packaging {}
 
 class StickerLabel <E extends Bag> {
   public void apply() { 
-    System.out.println("Applying Label to" + E );
+    System.out.println("Applying Label");
   }
 }  
+
+class Carrier { 
+  private List<Packaging> shipment = new ArrayList<>(); 
+
+
+  /** 
+      This method should not work because it extends.
+      because Extends creates an list could take a list of Packaging, List<Bag>, List<Box>
+      it will allow deletion... 
+  */
+  /** public void addToShipment(List<? extends Packaging> readyToShip) {
+    readyToShip.add(new Packaging());
+  }*/
+
+  public void addToShipment(List<? super Packaging> readyToShip) {
+    readyToShip.add(new Packaging());
+  }
+} 
 
 public class Packager { 
 
--- OCP17/chapter9/ReadMe.md
+++ OCP17/chapter9/ReadMe.md
@@ -4,3 +4,9 @@
 ### Dealing with Wild Cards. 
 - Important to note that the wildcards must match between class instances for example: 
   ` Packaging<Envelope> p1 = new Packaging<Bag>(); // Does not compile even if Envelope is a subclass`
+
+### Extends 
+- Objects can not be added to collection that extends because the compiler can not determine what the collection is of. 
+
+### Super
+- Objects can be added to Collections with super because everything can be cast to the super class. 
