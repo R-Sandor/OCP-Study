9268dd018d119c32656c81079f151788fde340a7
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-06-29 05:17:53 -0400

    Note on class declaration order

---
+++ OCP17/chapter7/DeclarationOrder.java
@@ -0,0 +1,4 @@
+// Declaration order of abstract is allow - not recommend - 
+// to be switched. This applies to sealed classes too.
+public abstract class DeclarationOrder {}
+abstract class ReverseOrder{}
